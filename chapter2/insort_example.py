a9e559d9ad087cf85370a25dcbe42a19aea7df99
Pavel Karateev <karateev.pavel@ya.ru>
2017-10-10 21:01:49 +0300

    insort example and tests for bisect

---
+++ chapter2/insort_example.py
@@ -0,0 +1,30 @@
+"""
+>>> random.seed(1729)
+>>> main()
+10 -> [10]
+ 0 -> [0, 10]
+ 6 -> [0, 6, 10]
+ 8 -> [0, 6, 8, 10]
+ 7 -> [0, 6, 7, 8, 10]
+ 2 -> [0, 2, 6, 7, 8, 10]
+10 -> [0, 2, 6, 7, 8, 10, 10]
+"""
+
+import bisect
+import random
+
+SIZE = 7
+
+random.seed(1729)
+
+
+def main():
+    my_list = []
+    for i in range(SIZE):
+        new_item = random.randrange(SIZE * 2)
+        bisect.insort(my_list, new_item)
+        print('%2d ->' % new_item, my_list)
+
+
+if __name__ == '__main__':
+    main()
